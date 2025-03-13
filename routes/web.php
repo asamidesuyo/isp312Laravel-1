33154f7bd507dc4e611bfff80163869f8ab3d046
unknown <admin@WEB1.web.local>
2025-03-13 10:16:00 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\User\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('index');
+//});
+
+Route::redirect('/', '/register');
+Route::get('/register', [RegisterController::class, 'index'])->name("register.index");
+Route::post('/register', [RegisterController::class, 'create'])->name("register.create");
